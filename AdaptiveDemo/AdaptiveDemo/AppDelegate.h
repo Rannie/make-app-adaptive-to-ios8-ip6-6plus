702c3339df0e747488038f2d5cbaa943cf0df4db
Hanran Liu <cocoarannie@163.com>
2014-11-17 16:38:34 +0800

    init

---
+++ AdaptiveDemo/AdaptiveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdaptiveDemo
+//
+//  Created by Ran on 14/11/17.
+//  Copyright (c) 2014年 Rannie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdaptiveDemo/AdaptiveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptiveDemo
+//
+//  Created by Ran on 14/11/17.
+//  Copyright (c) 2014年 Rannie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
